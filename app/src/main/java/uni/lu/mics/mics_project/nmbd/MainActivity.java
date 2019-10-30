33da5a627ebc95cf00d94509544ba12c3e03ae15
Nesryne Mej <arnoub.nemu@gmail.com>
2019-10-30 22:38:27 +0100

    Setup infrastructure + refactor imports

--- app/src/main/java/uni/lu/mics/mics_project/nmbd/MainActivity.java
+++ app/src/main/java/uni/lu/mics/mics_project/nmbd/MainActivity.java
@@ -1,34 +1,36 @@
 package uni.lu.mics.mics_project.nmbd;
 
-import uni.lu.mics.mics_project.nmbd.domain.model.User;
-import uni.lu.mics.mics_project.nmbd.service.Authentification;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.firebase.ui.auth.IdpResponse;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.annotations.Nullable;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
+
+import uni.lu.mics.mics_project.nmbd.domain.model.User;
+import uni.lu.mics.mics_project.nmbd.infra.DbManager;
+import uni.lu.mics.mics_project.nmbd.infra.repository.Factory;
+import uni.lu.mics.mics_project.nmbd.infra.repository.RepoCallback;
+import uni.lu.mics.mics_project.nmbd.infra.repository.RepoFacade;
+import uni.lu.mics.mics_project.nmbd.infra.repository.UserRepository;
+import uni.lu.mics.mics_project.nmbd.service.Authentification;
+import uni.lu.mics.mics_project.nmbd.service.ServiceFacade;
+import uni.lu.mics.mics_project.nmbd.service.ServiceFactory;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int MY_REQUEST_CODE = 123;
     //TAG used for Log and debugging
     final String TAG = "Main Activity";
-
-    private static final int MY_REQUEST_CODE = 123;
-    Authentification auth;
-
-    //reference to the database
-    private FirebaseFirestore mDatabase;
+    DbManager dbManager = new DbManager(new Factory());
+    RepoFacade repoFacade = dbManager.connect();
+    UserRepository userRepo = repoFacade.userRepo();
+    ServiceFacade serviceFacade = new ServiceFacade(new ServiceFactory());
+    Authentification authService = serviceFacade.authentificationService();
 
 
     @Override
