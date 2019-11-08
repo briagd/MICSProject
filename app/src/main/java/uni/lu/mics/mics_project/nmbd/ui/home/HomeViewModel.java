a19d66a85911e91e1dbf1f89e832d913adbe90dc
Nesryne Mej <arnoub.nemu@gmail.com>
2019-11-08 15:17:14 +0100

    Set up UI for ViewEvent Activity

---
+++ app/src/main/java/uni/lu/mics/mics_project/nmbd/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package uni.lu.mics.mics_project.nmbd.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
