d2d320ecdafb9345ff4195afb5c07edb620ba313
Nesryne Mej <arnoub.nemu@gmail.com>
2019-11-11 12:09:41 +0100

    Added Participants Activity

---
+++ app/src/main/java/uni/lu/mics/mics_project/nmbd/ParticipantsActivity.java
@@ -0,0 +1,46 @@
+package uni.lu.mics.mics_project.nmbd;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import uni.lu.mics.mics_project.nmbd.adapters.ParticipantsAdapter;
+
+public class Participants extends AppCompatActivity {
+
+    private static final String TAG = "ParticipantsActivity";
+
+    private ArrayList<String> mProfileNames = new ArrayList<>();
+    private ArrayList<String> mImagesUrl = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_participants);
+        Log.d(TAG, "onCreate: started");
+
+        initImageBitmaps();
+    }
+
+    private void initImageBitmaps(){
+        Log.d(TAG, "initImageBitmaps: preparing bitmaps.");
+
+        mImagesUrl.add("https://c1.staticflickr.com/5/46/36/253116407448_de5fbf183d_o.jpg");
+        mProfileNames.add("havasu falls");
+        mImagesUrl.add("https://i.redd.it/tpsnoz5bzo501.jpg");
+        mProfileNames.add("Trondheim");
+        initRecyclerView();
+    }
+
+    private void initRecyclerView(){
+        RecyclerView recyclerView = findViewById(R.id.participants_recycler_view);
+        ParticipantsAdapter adapter = new ParticipantsAdapter(this, mProfileNames, mImagesUrl);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
