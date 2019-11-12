dc0e6b9b5b07ac2dd33456b6344df35d9fa15f74
Nesryne Mej <arnoub.nemu@gmail.com>
2019-11-12 08:20:06 +0100

    setup description field made a user able to join an event

--- app/src/main/java/uni/lu/mics/mics_project/nmbd/EventActivity.java
+++ app/src/main/java/uni/lu/mics/mics_project/nmbd/EventActivity.java
@@ -3,12 +3,12 @@ package uni.lu.mics.mics_project.nmbd;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,17 +16,14 @@ import org.apache.commons.lang3.text.WordUtils;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 
 import uni.lu.mics.mics_project.nmbd.app.AppGlobalState;
 import uni.lu.mics.mics_project.nmbd.app.service.Images.ImageViewUtils;
-import uni.lu.mics.mics_project.nmbd.app.service.Storage;
 import uni.lu.mics.mics_project.nmbd.domain.model.Event;
 import uni.lu.mics.mics_project.nmbd.domain.model.User;
 import uni.lu.mics.mics_project.nmbd.infra.repository.EventRepository;
 import uni.lu.mics.mics_project.nmbd.infra.repository.RepoCallback;
-import uni.lu.mics.mics_project.nmbd.infra.repository.RepoMultiCallback;
 import uni.lu.mics.mics_project.nmbd.infra.repository.UserRepository;
 
 public class EventActivity extends AppCompatActivity {
@@ -43,6 +40,10 @@ public class EventActivity extends AppCompatActivity {
     private TextView place;
     private TextView eventType;
     private TextView host;
+    private TextView numberOfParticipants;
+    private TextView descriptionBox;
+    private Button joinLeaveBtn;
+    private Button editBtn;
     private User currentUser;
     private String currentUserID;
     private Event currentEvent;
@@ -61,87 +62,127 @@ public class EventActivity extends AppCompatActivity {
         Intent intent = getIntent();
         currentUser = (User) intent.getSerializableExtra("currentUser");
         currentUserID = currentUser.getId();
-        currentEvent =  (Event) intent.getSerializableExtra("currentEvent");
+        currentEvent = (Event) intent.getSerializableExtra("currentEvent");
 //        Check if there is an image uri,this would happen if the event was just created and the picture is still being
 //        uploaded
 //        , otherwise display the picture from the eventID.
 //        imgView: is the imageView object where you want the picture to be displayed
 
+        setJoinLeaveButton();
+        setEditButton();
 
-        if(intent.hasExtra("image")){
+        if (intent.hasExtra("image")) {
             Uri imageUri = Uri.parse(intent.getStringExtra("image"));
 
-            ImageViewUtils.displayPicUri(this,imageUri,imgView);
+            ImageViewUtils.displayPicUri(this, imageUri, imgView);
         } else {
             ImageViewUtils.displayEventPicID(this, currentEvent.getId(), imgView);
         }
-                setEventName(currentEvent.getName());
-                setDate(currentEvent.getDate());
-                setAdress(currentEvent.getEventAddress());
-                setCategory(currentEvent.getCategory());
-                String creatorId = currentEvent.getCreator();
-                Log.d(TAG, "Listing Users");
-                userRepo.list(new RepoMultiCallback<User>() {
-                    @Override
-                    public void onCallback(ArrayList<User> users) {
-                        for (User u : users) {
-                            Log.d(TAG, "listing user: " + u.getId());
-                            Toast.makeText(EventActivity.this, u.getName(), Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
+        setEventName(currentEvent.getName());
+        setDate(currentEvent.getDate());
+        setAdress(currentEvent.getEventAddress());
+        setCategory(currentEvent.getCategory());
+        String creatorId = currentEvent.getCreator();
+        Log.d(TAG, "Listing Users");
+        userRepo.findById(creatorId, new RepoCallback<User>() {
+            @Override
+            public void onCallback(User user) {
+                setHost(user.getName());
+                Log.d(TAG, "Setting Event creator's Name");
+                //setHost("Hosted by " + model.getName());
+                //Toast.makeText(EventActivity.this, model.getName(), Toast.LENGTH_LONG).show();
+            }
+        });
+        setNumberOfParticipants(currentEvent.getEventParticipants().size());
+        setDescription(currentEvent.getDescription());
 
 
-                userRepo.findById(creatorId, new RepoCallback<User>() {
-                    @Override
-                    public void onCallback(User user) {
-                        host=(TextView)findViewById(R.id.hosted_by);
-                        host.setText(user.getName());
-                        Log.d(TAG, "I am finding the user");
-                        //setHost("Hosted by " + model.getName());
-                        //Toast.makeText(EventActivity.this, model.getName(), Toast.LENGTH_LONG).show();
-                    }
-            });
-
-        Toast.makeText(EventActivity.this, currentEvent.getId(), Toast.LENGTH_LONG).show();
+        //Toast.makeText(EventActivity.this, currentEvent.getId(), Toast.LENGTH_LONG).show();
     }
 
-    private void setEventName(String name){
+    private void setEventName(String name) {
         eventTitle = (TextView) findViewById(R.id.eventTitle);
         eventTitle = (TextView) findViewById(R.id.eventTitle);
         eventTitle.setText(name);
     }
 
-    private void setDate(String dateString){
+    private void setDate(String dateString) {
         SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
         SimpleDateFormat formatterToStr = new SimpleDateFormat("EEEE, MMMM dd");
-        dateField = (TextView)findViewById(R.id.day_monthId);
+        dateField = (TextView) findViewById(R.id.day_monthId);
         try {
             Date date = formatter.parse(dateString);
             dateField.setText(formatterToStr.format(date));
-        }catch (ParseException e) {
+        } catch (ParseException e) {
             e.printStackTrace();
         }
 
     }
 
-    private void setAdress(String adress){
-        place = (TextView)findViewById(R.id.placeId);
+    private void setAdress(String adress) {
+        place = (TextView) findViewById(R.id.placeId);
         place.setText(adress);
     }
 
-    private  void setCategory(String category){
+    private void setCategory(String category) {
         eventType = (TextView) findViewById(R.id.categoryId);
         eventType.setText(WordUtils.capitalize(category.toLowerCase()));
     }
 
-    private void setHost(String hostName){
-        host = (TextView)findViewById(R.id.hosted_by);
-        host.setText(hostName);
+    private void setHost(String hostName) {
+        host = (TextView) findViewById(R.id.hosted_by);
+        host.setText("Hosted by " + hostName);
 
     }
 
-//         Uncomment to be able to pass user as intent when back button is pressed
+    private void setNumberOfParticipants(int number) {
+        numberOfParticipants = findViewById(R.id.number_peeps_going);
+        if (number == 1) {
+            numberOfParticipants.setText(number + " person is going");
+        } else {
+            numberOfParticipants.setText(number + " people are going");
+        }
+
+    }
+
+    private void setDescription(String description) {
+        descriptionBox = findViewById(R.id.description_box);
+        descriptionBox.setMovementMethod(new ScrollingMovementMethod());
+        descriptionBox.setText(description);
+
+    }
+
+    private void setJoinLeaveButton() {
+        joinLeaveBtn = (Button) findViewById(R.id.participateBtn);
+        if (currentEvent.getEventParticipants().contains(currentUser.getId())) {
+            joinLeaveBtn.setText("Leave");
+        } else {
+            joinLeaveBtn.setText("Join");
+        }
+    }
+
+    private void setEditButton() {
+        editBtn = findViewById(R.id.edit_event);
+        if (currentEvent.getEventAdmins().contains(currentUser.getId())) {
+            editBtn.setVisibility(View.VISIBLE);
+        } else {
+            editBtn.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    public void joinLeaveEvent(View view) {
+        if (joinLeaveBtn.getText() == "Join") {
+            currentEvent.getEventParticipants().add(currentUser.getId());
+            eventRepo.addElement(currentEvent.getId(), "eventParticipants", currentUser.getId());
+            joinLeaveBtn.setText("Leave");
+        } else {
+            currentEvent.getEventParticipants().remove(currentUser.getId());
+            eventRepo.removeElement(currentEvent.getId(), "eventParticipants", currentUser.getId());
+            joinLeaveBtn.setText("Join");
+        }
+    }
+
+    //         Uncomment to be able to pass user as intent when back button is pressed
     @Override
     public void onBackPressed() {
         Log.d(TAG, "onBackPressed Called");
